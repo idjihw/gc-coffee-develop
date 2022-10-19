f0bb21f06a5fcf6d4e92a72f860cbcf3a6fb167a
idjihw <redchoco06@gmail.com>
2022-10-19 21:10:14 +0900

    오류 수정, order 수정

--- src/main/java/com/example/gccoffee/repository/OrderJdbcRepository.java
+++ src/main/java/com/example/gccoffee/repository/OrderJdbcRepository.java
@@ -24,12 +24,12 @@ public class OrderJdbcRepository implements OrderRepository {
     @Transactional
     public Order insert(Order order) {
         jdbcTemplate.update("INSERT INTO orders(order_id, email, address, postcode, order_status, created_at, updated_at) " +
-                        "VALUES (UNHEX(REPLACE(:orderId, '-', ''), :email, :address, :postcode, :orderStatus, :createdAt, :updatedAt)",
+                        "VALUES (UNHEX(REPLACE(:orderId, '-', '')), :email, :address, :postcode, :orderStatus, :createdAt, :updatedAt)",
                 toOrderParamMap(order));
         order.getOrderItems()
                 .forEach(item ->
                         jdbcTemplate.update("INSERT INTO order_items(order_id, product_id, category, price, quantity, created_at, updated_at) " +
-                                        "VALUES (UNHEX(REPLACE(:orderId, '-', ''), UNHEX(REPLACE(:productId, '-', ''), :category, :price, :quantity, :createdAt, :updatedAt)",
+                                        "VALUES (UNHEX(REPLACE(:orderId, '-', '')), UNHEX(REPLACE(:productId, '-', '')), :category, :price, :quantity, :createdAt, :updatedAt)",
                                 toOrderItemParamMap(order.getOrderId(), order.getCreatedAt(), order.getUpdatedAt(), item)));
         return order;
     }
